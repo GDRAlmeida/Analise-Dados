0a68973bbb20bc1455a47dd8272d9f80329896a8
Pedro Henrique Costa <82903937+pedrohncosta@users.noreply.github.com>
2024-04-16 19:19:12 -0300

    Update minhaBiblioteca.py
    Corrigindo o código

--- minhaBiblioteca.py
+++ minhaBiblioteca.py
@@ -1,6 +1,4 @@
 class MinhaClasse:
     @staticmethod
     def autores():
-        print("Autores:\nNome: Gabriel Davi Reis Almeida\nMatrícula: 204400057\n
-                         Nome: Larissa Aparecida Leite da Silva\nMatrícula: 184450017\n
-                         Nome: Pedro Henrique do Nascimento Costa\nMatrícula: 174450033")
+        print("Autores:\nNome: Gabriel Davi Reis Almeida\nMatrícula: 204400057\nNome: Larissa Aparecida Leite da Silva\nMatrícula: 184450017\nNome: Pedro Henrique do Nascimento Costa\nMatrícula: 174450033")
