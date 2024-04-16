5e5dd1dd0b2639b100808c2f3aac6ff4fbef1b95
Pedro Henrique Costa <82903937+pedrohncosta@users.noreply.github.com>
2024-04-16 19:18:25 -0300

    Update minhaBiblioteca.py
    Add Pedro and Larissa

--- minhaBiblioteca.py
+++ minhaBiblioteca.py
@@ -1,4 +1,6 @@
 class MinhaClasse:
     @staticmethod
     def autores():
-        print("Autores:\nNome: Gabriel Davi Reis Almeida\nMatrícula: 204400057")
+        print("Autores:\nNome: Gabriel Davi Reis Almeida\nMatrícula: 204400057\n
+                         Nome: Larissa Aparecida Leite da Silva\nMatrícula: 184450017\n
+                         Nome: Pedro Henrique do Nascimento Costa\nMatrícula: 174450033")
