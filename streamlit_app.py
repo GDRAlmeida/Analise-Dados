606120745e0406e0162cd635bfcedd4f4cedfda1
GDRAlmeida <166956649+GDRAlmeida@users.noreply.github.com>
2024-04-18 14:25:52 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,12 @@
 import streamlit as st
 import pandas as pd
-
-# Lendo o arquivo CSV
-df = pd.read_csv('Dados_Diabetes.csv', sep=';')
+from Main import df
 
 # Criando o gráfico de pizza
 st.title('Gráfico de Pizza para Diabetes')
 
 # Contando os valores únicos na coluna 'Diabetes'
-diabetes_counts = df['Diabetes'].value_counts()
+diabetes_counts = df['diabetes'].value_counts()
 
 # Exibindo o gráfico de pizza
 st.write(diabetes_counts)
