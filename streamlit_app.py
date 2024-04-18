df180b5f24348fe9bb9be9c4dec9c490d417dd72
GDRAlmeida <166956649+GDRAlmeida@users.noreply.github.com>
2024-04-18 14:39:24 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,35 +1,28 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Lendo o arquivo CSV
 df = pd.read_csv('Dados_Diabetes.csv', sep=';')
+
+# Imprimindo o DataFrame para depuração
+st.write(df)
 
 # Criando o gráfico de pizza
 st.title('Gráfico de Pizza para Diabetes')
 
 # Verificando se a coluna 'diabetes' está presente
 if 'diabetes' in df.columns:
-    # Separando os dados de diabéticos com valores 0 e 1
-    diabetes_0 = df[df['diabetes'] == 0]
-    diabetes_1 = df[df['diabetes'] == 1]
+    # Contando os valores únicos na coluna 'Diabetes'
+    diabetes_counts = df['diabetes'].value_counts()
     
-    # Contando os valores únicos em cada série
-    diabetes_counts_0 = diabetes_0.shape[0]
-    diabetes_counts_1 = diabetes_1.shape[0]
-
-    # Exibindo os contagens
-    st.write("Contagem de Diabéticos (Diabetes = 0):", diabetes_counts_0)
-    st.write("Contagem de Diabéticos (Diabetes = 1):", diabetes_counts_1)
-
-    # Dados para plotagem
-    labels = ['Diabetes = 0', 'Diabetes = 1']
-    sizes = [diabetes_counts_0, diabetes_counts_1]
-
-    # Cria um gráfico de pizza
-    plt.pie(sizes, labels=labels, autopct='%1.1f%%')
- 
-    # Exibe o gráfico no Streamlit
-    st.pyplot(plt)
+    # Exibindo o gráfico de pizza
+    st.write(diabetes_counts)
 else:
     st.error("A coluna 'Diabetes' não foi encontrada no DataFrame.")
+
+fig, ax = plt.subplots()
+ax.hist(diabetes_counts, bins=20)
+
+st.pyplot(fig)
