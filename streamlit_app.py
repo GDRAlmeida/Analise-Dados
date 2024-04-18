fa1d0650653fa288ba7827535684869945c28fda
GDRAlmeida <166956649+GDRAlmeida@users.noreply.github.com>
2024-04-18 14:27:59 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,21 @@
 import streamlit as st
 import pandas as pd
-from Main import df
+
+# Lendo o arquivo CSV
+df = pd.read_csv('Dados_Diabetes.csv', sep=';')
+
+# Imprimindo o DataFrame para depuração
+st.write(df)
 
 # Criando o gráfico de pizza
 st.title('Gráfico de Pizza para Diabetes')
 
-# Contando os valores únicos na coluna 'Diabetes'
-diabetes_counts = df['diabetes'].value_counts()
-
-# Exibindo o gráfico de pizza
-st.write(diabetes_counts)
+# Verificando se a coluna 'Diabetes' está presente
+if 'Diabetes' in df.columns:
+    # Contando os valores únicos na coluna 'Diabetes'
+    diabetes_counts = df['Diabetes'].value_counts()
+    
+    # Exibindo o gráfico de pizza
+    st.write(diabetes_counts)
+else:
+    st.error("A coluna 'Diabetes' não foi encontrada no DataFrame.")
