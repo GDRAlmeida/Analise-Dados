46d24ead2572b1e4cc50ecd525fc0e3b655dc3a4
GDRAlmeida <166956649+GDRAlmeida@users.noreply.github.com>
2024-04-18 14:29:50 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,18 +4,19 @@ import pandas as pd
 # Lendo o arquivo CSV
 df = pd.read_csv('Dados_Diabetes.csv', sep=';')
 
-# Imprimindo o DataFrame para depuração
-st.write(df)
-
 # Criando o gráfico de pizza
 st.title('Gráfico de Pizza para Diabetes')
 
 # Verificando se a coluna 'diabetes' está presente
 if 'diabetes' in df.columns:
-    # Contando os valores únicos na coluna 'Diabetes'
+    # Contando os valores únicos na coluna 'diabetes'
     diabetes_counts = df['diabetes'].value_counts()
     
     # Exibindo o gráfico de pizza
     st.write(diabetes_counts)
+
+    # Criando o gráfico de pizza com as informações de contagem
+    st.write("### Distribuição de Diabetes")
+    st.plotly_chart(diabetes_counts.plot.pie(autopct='%1.1f%%', figsize=(8, 8)))
 else:
     st.error("A coluna 'Diabetes' não foi encontrada no DataFrame.")
