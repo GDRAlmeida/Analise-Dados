5085a5fd8e795d1ba36eaf1eb60c92dcf51638cc
GDRAlmeida <166956649+GDRAlmeida@users.noreply.github.com>
2024-04-18 14:50:40 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,18 +5,18 @@ import pandas as pd
 df = pd.read_csv('Dados_Diabetes.csv', sep=';')
 
 # Criando o layout de grade
-col1, _, col2 = st.columns([2, 0.1, 1])
+col1, col2, col3 = st.columns([2,1,1])
 
 # Exibindo a depuração na primeira coluna
 with col1:
     st.title('Depuração')
     st.write(df)
-
-# Adicionando um espaço entre as colunas
-st.write('')
+    
+with col2:
+    st.write('')
 
 # Criando o gráfico na segunda coluna
-with col2:
+with col3:
     st.title('Gráfico para Diabetes')
 
     # Verificando se a coluna 'diabetes' está presente
@@ -24,8 +24,8 @@ with col2:
         # Contando os valores únicos na coluna 'Diabetes'
         diabetes_counts = df['diabetes'].value_counts()
         
-        # Exibindo o gráfico de barras com tons de cinza
-        st.bar_chart(diabetes_counts, use_container_width=True)
+        # Exibindo o gráfico de pizza
+        st.bar_chart(diabetes_counts)
         st.write(diabetes_counts)
     else:
         st.error("A coluna 'Diabetes' não foi encontrada no DataFrame.")
