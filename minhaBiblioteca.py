1e717b6abf94bbca678a95295a75570510ce2712
Pedro Henrique Costa <82903937+pedrohncosta@users.noreply.github.com>
2024-04-18 21:13:11 -0300

    Update minhaBiblioteca.py
    Add new names

--- minhaBiblioteca.py
+++ minhaBiblioteca.py
@@ -1,4 +1,4 @@
 class MinhaClasse:
     @staticmethod
     def autores():
-        print("Autores:\nNome: Gabriel Davi Reis Almeida\nMatrícula: 204400057\nNome: Larissa Aparecida Leite da Silva\nMatrícula: 184450017\nNome: Pedro Henrique do Nascimento Costa\nMatrícula: 174450033")
+        print("Autores:\nNome: Gabriel Davi Reis Almeida\nMatrícula: 204400057\nNome: Larissa Aparecida Leite da Silva\nMatrícula: 184450017\nNome: Pedro Henrique do Nascimento Costa\nMatrícula: 174450033\nNome: Danielly Mendonça Sousa\nMatricula:194450048\nNome: Lincoln Vitor Ruela Oliveira\nMatricula: 164350058")
